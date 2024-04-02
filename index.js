9b010f107f0f518fe039099b03c34577e3a4b74d
razzzz1 <ryanmaiyo1@gmail.com>
2024-04-02 11:36:23 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
-// Write your code here
+var num1 =31;
+var num2 =2;
+const multiply =  num1 * num2
+console.log(multiply)
+const random = Math.ceil(Math.random() * 26)
+console.log(random)
+var num3 =22;
+var num4 =9;
+const mod = num3 % num4
+console.log(mod)
+const max = Math.max(5, 10, 15, 20)
+console.log(max)
